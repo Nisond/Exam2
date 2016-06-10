c45b6249f479dd0d511196b351b48f2bf2632e38
Nisond <nisklausen@hotmail.com>
2016-06-10 09:05:36 -0400

    Add files via upload

---
+++ PackageExam2016/R/Task31.R
@@ -0,0 +1,35 @@
+#' @export
+#' @title Task31
+#' @return Returns an estimated p-value for some variable.
+#' @keywords chisq
+#' @description This is a function to estimates the p-value
+#' @param n number of tests
+#' @param df degrees of freedom
+#' @param x the variable that the q-square test is done upon.
+#' @author Nis Klausen \cr
+#' Department of mathematics and computer science (IMADA) \cr
+#' University of southern Denmark, SDU, Odense \cr
+#' \email{nklau1414@student.sdu.dk} \cr
+
+
+Task31 <- function(x,df,n=1000){
+
+  #Syntax Processing
+  #============================================
+  if(length(x)!=1 || !is.numeric(x)){
+    stop("x must be a number of length 1")
+  }
+  # if(length(df)!=1 || df!=as.integer(df)|| df < 0 ){
+  #   stop("degrees of freedom must be a single positive integer")
+  # }
+  # if(length(n)!=1 || df!=as.integer(n)|| n < 0 ){
+  #   stop("n must be a single positive integer")
+  # }
+
+  #--------------------------------------------
+  var <- numeric(n)
+  var <- sapply(var, function(var) var+sum(rnorm(df)^2))
+  pvalue <- sum(var>x)/n
+  return(pvalue)
+
+}
